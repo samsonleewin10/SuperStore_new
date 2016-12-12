28db85a741a9d059284a5f037ad9e9ad765351c9
samsonleewin10 <microsoftdx@Microsoft-DXs-MacBook-Pro.local>
2016-12-12 13:24:40 +0800

    First Commit

---
+++ SuperStore/SuperStore/Controllers/RecordsController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SuperStore.Models;
+using Microsoft.ApplicationInsights;
+
+namespace SuperStore.Controllers
+{
+    public class RecordsController : Controller
+    {
+		private ApplicationDbContext db = new ApplicationDbContext();
+
+		private TelemetryClient telemetry = new TelemetryClient();
+
+        public ActionResult Index()
+        {
+			telemetry.TrackEvent("View Purchase Record");
+			return View (db.Records.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View (record);
+        }
+
+        public ActionResult Create()
+        {
+			telemetry.TrackEvent("Attempt to Purchase");
+            return View ();
+        } 
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try {
+				telemetry.TrackEvent("Made a Purchase");
+				db.Records.Add(record);
+				db.SaveChanges();
+                return RedirectToAction ("Index");
+            } catch {
+                return View ();
+            }
+        }
+        
+        public ActionResult Edit(int id)
+        {
+            return View ();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try {
+				db.Entry(record).State = EntityState.Modified;
+				db.SaveChanges();
+                return RedirectToAction ("Index");
+            } catch {
+                return View (record);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View ();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try {
+                return RedirectToAction ("Index");
+            } catch {
+                return View (record);
+            }
+        }
+    }
+}
---
+++ SuperStore/SuperStore/Models/Record.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SuperStore.Models
+{
+	public class Record
+	{
+		public int RecordId { get; set; }
+
+		[DataType(DataType.Date)]
+		public string Date { get; set; }
+
+		public string CustomerName { get; set; }
+
+		public string ProductName { get; set; }
+
+		public int Quantity { get; set; }
+
+		public string DeliveryAddress { get; set; }
+
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+
+	}
+}
